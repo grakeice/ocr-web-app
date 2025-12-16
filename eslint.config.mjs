f14d532002ced25a4015638ba95669220985baf7
kurage <89824152+grakeice@users.noreply.github.com>
2025-12-16 11:55:50 +0900

    project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+// @ts-check
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import oxlint from "eslint-plugin-oxlint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+	...nextVitals,
+	...nextTs,
+	// Override default ignores of eslint-config-next.
+	globalIgnores([
+		// Default ignores of eslint-config-next:
+		".next/**",
+		"out/**",
+		"build/**",
+		"next-env.d.ts",
+	]),
+	eslintConfigPrettier,
+	oxlint.configs["flat/all"],
 ]);
 
 export default eslintConfig;
