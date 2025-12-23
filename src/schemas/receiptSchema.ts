11838f06751eb148506bff8c4de786180648cc1d
kurage <89824152+grakeice@users.noreply.github.com>
2025-12-24 01:57:58 +0900

    add discount field into receiptSchema

--- src/schemas/receiptSchema.ts
+++ src/schemas/receiptSchema.ts
@@ -5,8 +5,11 @@ export const receiptSchema = z.array(
 		name: z.string().describe("商品名"),
 		price: z.number().describe("商品ひとつあたりの値段"),
 		count: z.number().describe("商品の個数"),
+		discount: z.number().describe("商品の合計値引き額"),
 		totalPrice: z
 			.number()
-			.describe("その商品の合計の値段(priceとcountの積)"),
+			.describe(
+				"その商品の合計の値段(priceとcountの積からdiscountを引いたもの)",
+			),
 	}),
 );
